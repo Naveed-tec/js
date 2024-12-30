1a790202242c030147341e1d90d891f8530fd17a
Muhammad Naveed <mu42ghal.33@gmail.com>
2024-12-30 09:05:05 +0000

    conversion of datatype

---
+++ DatatypeConversion.js
@@ -0,0 +1,15 @@
+let score = "100abc"
+console.log(typeof score)
+let valInScore = Number(score)
+console.log(typeof valInScore);
+console.log(valInScore);
+
+let isLoggedIn = 1
+let booleanIs = Boolean(isLoggedIn)
+console.log(booleanIs);
+
+let num = 33
+let numString = String(num)
+console.log(typeof numString);
+console.log(numString);
+
