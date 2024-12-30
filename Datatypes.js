281b49a588436d6ae68cb41bc00656bde1978706
Muhammad Naveed <mu42ghal.33@gmail.com>
2024-12-30 08:02:18 +0000

    datatype

---
+++ Datatypes.js
@@ -0,0 +1,7 @@
+//"use strict";  //treat all js code as newer version
+//string
+//number
+//boolean
+//undefined => typeof undefined is also undefined
+//null => typeof null is object in js
+//bigInt
