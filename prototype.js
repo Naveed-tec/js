726c693aef9ebdbaf9714d5141c3f0c887ef2242
Muhammad Naveed <mu42ghal.33@gmail.com>
2025-01-01 16:54:03 +0500

    Add files via upload
    prototypes

---
+++ prototype.js
@@ -0,0 +1,24 @@
+// let gameName = new String("naveed")
+// console.log(typeof gameName)
+// let nameChange = gameName
+// console.log(nameChange.toUpperCase())
+// console.log(gameName.__proto__)
+// console.log(gameName.charAt(3))
+
+let gameName = new String("naveed-mughal")
+let newString = gameName.substring(0,4)
+console.log(newString)
+
+let newStringOne = gameName.slice(2,8)
+console.log(newStringOne)
+
+let userName = ("    Naveed    ")
+console.log(userName)
+console.log(userName.trim())
+
+let myemail = "https://naveedmughal%20com"
+console.log(myemail.replace('%20','-'))
+
+console.log(gameName.blink())
+console.log(gameName.bold())
+console.log(gameName.)
