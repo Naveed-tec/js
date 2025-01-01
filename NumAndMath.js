a740628a382d3b79eaa13d69b6adea0dc78fa7ca
Muhammad Naveed <mu42ghal.33@gmail.com>
2025-01-01 22:12:36 +0500

    Add files via upload
    Math methods

---
+++ NumAndMath.js
@@ -0,0 +1,12 @@
+// let num = 344.634
+// console.log(num.toFixed(1))
+// console.log(num.toPrecision(3))
+
+// let num2 = 10000000
+// console.log(num2.toLocaleString('en-IN')) 
+
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
