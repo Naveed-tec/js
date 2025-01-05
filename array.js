90c5933936d6933157cbc30f3ab034f7008fb481
Muhammad Naveed <mu42ghal.33@gmail.com>
2025-01-05 19:58:08 +0500

    Add files via upload
    all about arrays

---
+++ array.js
@@ -0,0 +1,36 @@
+// let arr = [2,4,6,7,8]
+// arr.push(9)
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+
+// arr.unshift(1)
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+
+// let arr = [1,2,3,4,5,6,7,8]
+// const a_splice = arr.splice(2,5)
+// console.log(a_splice);
+// console.log(arr);
+
+// let new_arr = [1,2,3,4,5,6,7,8]
+// const b_slice = new_arr.slice(2,5)
+// console.log(b_slice);
+// console.log(new_arr);
+
+
+let arr_one = ["Spidey", "Thor", "Loki"]
+let arr_two = ["Hulk", "Batman", "Thianos"]
+
+// let combine_arr = arr_one.concat(arr_two)
+// console.log(combine_arr);
+
+// let combine_arr = [...arr_one, ...arr_two]
+// console.log(combine_arr);
+
+
+console.log(Array.isArray("Naveed"))
+console.log(Array.of("Naveed","Mughal"))
+console.log(Array.from("Naveed"));
+
