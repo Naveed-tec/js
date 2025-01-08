3f981bbf9c483cda10e4c6991665980ef135219a
Muhammad Naveed <mu42ghal.33@gmail.com>
2025-01-08 11:35:30 +0500

    Update object.js
    new code added

--- object.js
+++ object.js
@@ -1,4 +1,18 @@
 let mysym = Symbol("Hello")
+let mysymobj = Object(mysym)
+let mysymstr = String(mysym)
+// console.log(typeof mysym)
+// console.log(typeof mysymobj)
+// console.log(typeof mysymstr)
+
+// let  sym1 = Symbol("World")
+// let  sym2 = Symbol("World")
+// console.log(sym1 === sym2)
+
+let  sym1 = Symbol.for("World")
+let  sym2 = Symbol.for("World")
+console.log(sym1 === sym2)
+                             //Objects as literals
 let JsUser = {
     [mysym] : "Hello World",
     name : "Naveed",
@@ -7,4 +21,35 @@ let JsUser = {
     location : "Muridke",
     isLoogedIn : false
 }
+// Object.freeze(JsUser)
+// JsUser.email = "naveedzulfiqar102@gmail.com"
 console.log(typeof JsUser[mysym])
+
+JsUser.greetings = function(){
+    console.log("Hello World")
+}
+JsUser.greetings()
+
+JsUser.greetingsTwo = function(){
+    return `Hello User, ${this.name}`
+}
+console.log(JsUser.greetingsTwo())
+
+                                     //Objects as constructor or singleton
+let newUser = new Object()
+newUser.id = "Naveed"
+newUser.age = "22"
+newUser.email = "naveedzulfiqar100@gmail.com"
+// console.log(newUser)
+
+// let obj1 = {1: "a", 2: "b"}
+// let obj2 = {3: "c", 4: "d"}
+
+// let obj3 = Object.assign({}, obj1, obj2)
+
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+console.log(Object.keys(newUser))
+console.log(Object.values(newUser))
+console.log(Object.entries(newUser))
