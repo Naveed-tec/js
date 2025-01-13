179280ada424ee3bc634f09f5e0225b80243b2e6
Muhammad Naveed <mu42ghal.33@gmail.com>
2025-01-13 21:40:30 +0500

    Add files via upload
    scope explanation

---
+++ scopes.js
@@ -0,0 +1,18 @@
+let age = 22
+if(true){
+    // console.log(age)
+    let newage = 24
+}
+// console.log(newage)
+
+function one(){
+    const name = "Naveed"
+    // console.log(name)
+    function two(){
+        const sirname = "Mughal"
+        console.log(name)
+    }
+    two()
+    // console.log(sirname)
+}
+one()
